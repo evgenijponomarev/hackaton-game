1ba615eb75b3d243c3dfabe7689266d97daf4c6d
Evgenij Ponomarev <evgenij.ponomarev@icloud.com>
2025-10-05 15:15:06 +0300

    Add mobile buttons

--- src/entities/player.ts
+++ src/entities/player.ts
@@ -3,6 +3,7 @@ import { Entity } from './entitiy';
 export class Player extends Entity {
   textureKey: string;
   private moveSpeed = 50;
+  private shifting: 'left' | 'right' | null = null;
 
   constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
     super(scene, x, y, texture);
@@ -36,20 +37,32 @@ export class Player extends Entity {
     // При 60 FPS delta = 16.67ms, при 30 FPS delta = 33.33ms
     const normalizedDelta = delta * (delta / 16.67); // Нормализация к 60 FPS
 
-    const velocity = { x: 0, y: -this.moveSpeed * normalizedDelta };
+    const defaultVelocity = { x: 0, y: -this.moveSpeed * normalizedDelta };
 
-    if (keys?.left.isDown) {
-      velocity.x = -this.moveSpeed * normalizedDelta;
+    if (keys?.left.isDown || this.shifting === 'left') {
+      defaultVelocity.x = -this.moveSpeed * normalizedDelta;
     }
 
-    if (keys?.right.isDown) {
-      velocity.x = this.moveSpeed * normalizedDelta;
+    if (keys?.right.isDown || this.shifting === 'right') {
+      defaultVelocity.x = this.moveSpeed * normalizedDelta;
     }
 
-    if (velocity.x === 0 && velocity.y === 0) {
+    if (defaultVelocity.x === 0 && defaultVelocity.y === 0) {
       this.stopRun();
     }
 
-    this.setVelocity(velocity.x, velocity.y);
+    this.setVelocity(defaultVelocity.x, defaultVelocity.y);
+  }
+
+  shiftLeft() {
+    this.shifting = 'left';
+  }
+
+  shiftRight() {
+    this.shifting = 'right';
+  }
+
+  stopShifting() {
+    this.shifting = null;
   }
 }
--- src/scenes/track.ts
+++ src/scenes/track.ts
@@ -6,57 +6,130 @@ export class Track extends Phaser.Scene {
   player?: Player;
   score = 0;
   scoreText?: Phaser.GameObjects.Text;
+  map?: Phaser.Tilemaps.Tilemap;
+  layers?: Phaser.Tilemaps.TilemapLayer[];
+  buttonLeft?: Phaser.GameObjects.Rectangle;
+  buttonRight?: Phaser.GameObjects.Rectangle;
 
   constructor() {
     super('TrackScene');
   }
 
-  preload() {
+  loadAssets() {
     trackJSON.tilesets.forEach((tileset) => {
       tileset.tiles.forEach((tile) => {
         this.load.image(tile.image, tile.image.replace('../../public/', '/'));
       });
     });
 
-    this.load.tilemapTiledJSON(TILEMAP_NAME, 'src/map/track.json?');
-
     this.load.spritesheet(SPRITE.PLAYER, '/assets/images/player.png', {
       frameWidth: SIZE.PLAYER.WIDTH,
       frameHeight: SIZE.PLAYER.HEIGHT,
     });
+
+    this.load.tilemapTiledJSON(TILEMAP_NAME, 'src/map/track.json?');
   }
 
-  create() {
-    const map = this.make.tilemap({ key: TILEMAP_NAME });
-
-    const tilesets = map.tilesets
-      .map((tileset) => {
-        return map.addTilesetImage(tileset.name, tileset.name);
-      })
-      .filter((t) => !!t);
-
-    const layers = trackJSON.layers
-      .map((layer) => {
-        return map.createLayer(layer.name, tilesets);
-      })
-      .filter((l) => !!l);
-
-    this.player = new Player(
-      this,
-      map.widthInPixels / 2,
-      map.heightInPixels - SIZE.PLAYER.HEIGHT,
-      SPRITE.PLAYER
-    );
+  addScoreText() {
+    if (!this.map) return;
 
     this.scoreText = this.add.text(0, 0, `Score: ${this.score}`, {
       fontSize: 36,
       align: 'center',
-      fixedWidth: map.widthInPixels,
+      fixedWidth: this.map.widthInPixels,
       color: '#fff',
       backgroundColor: '#000',
       padding: { x: 0, y: 10 },
     });
     this.scoreText.setScrollFactor(0);
+  }
+
+  addButton(type: 'left' | 'right') {
+    if (!this.map || !this.player) return;
+
+    const button = this.add.rectangle(
+      type === 'left' ? 0 : this.sys.game.canvas.width,
+      this.sys.game.canvas.height,
+      400,
+      400,
+      0xffffff,
+      0.5
+    );
+    button.setRounded(100);
+    button.setScrollFactor(0);
+
+    // Делаем кнопку интерактивной
+    button.setInteractive({ useHandCursor: true });
+
+    // Обработчики для мыши и тач-устройств
+    const startAction =
+      type === 'left' ? this.player.shiftLeft : this.player.shiftRight;
+    const stopAction = this.player.stopShifting;
+
+    // Визуальная обратная связь при нажатии
+    button.on('pointerdown', () => {
+      button.setAlpha(0.8); // Затемняем кнопку при нажатии
+      startAction.call(this.player);
+    });
+
+    button.on('pointerup', () => {
+      button.setAlpha(0.5); // Возвращаем прозрачность
+      stopAction.call(this.player);
+    });
+
+    button.on('pointerout', () => {
+      button.setAlpha(0.5); // Возвращаем прозрачность при выходе курсора
+      stopAction.call(this.player);
+    });
+
+    // События для тач-устройств
+    button.on('touchstart', () => {
+      button.setAlpha(0.8);
+      startAction.call(this.player);
+    });
+
+    button.on('touchend', () => {
+      button.setAlpha(0.5);
+      stopAction.call(this.player);
+    });
+
+    button.on('touchcancel', () => {
+      button.setAlpha(0.5);
+      stopAction.call(this.player);
+    });
+
+    return button;
+  }
+
+  createMap() {
+    this.map = this.make.tilemap({ key: TILEMAP_NAME });
+
+    const tilesets = this.map.tilesets
+      .map((tileset) => {
+        return this.map!.addTilesetImage(tileset.name, tileset.name);
+      })
+      .filter((t) => !!t);
+
+    this.layers = trackJSON.layers
+      .map((layer) => {
+        return this.map!.createLayer(layer.name, tilesets);
+      })
+      .filter((l) => !!l);
+  }
+
+  createPlayer() {
+    if (!this.map) return;
+
+    this.player = new Player(
+      this,
+      this.map.widthInPixels / 2,
+      this.map.heightInPixels - SIZE.PLAYER.HEIGHT,
+      SPRITE.PLAYER
+    );
+  }
+
+  initCamera() {
+    if (!this.player || !this.map) return;
 
     this.cameras.main.startFollow(
       this.player,
@@ -64,18 +137,44 @@ export class Track extends Phaser.Scene {
       1,
       1,
       0,
-      window.innerHeight / 2
+      this.sys.game.canvas.height / 3
     );
-    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-    this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.setBounds(
+      0,
+      0,
+      this.map.widthInPixels,
+      this.map.heightInPixels
+    );
+  }
+
+  initWorldPhisics() {
+    if (!this.player || !this.map) return;
+
+    this.physics.world.setBounds(
+      0,
+      0,
+      this.map.widthInPixels,
+      this.map.heightInPixels
+    );
     this.player.setCollideWorldBounds(true);
-    const roadsideObjectsLayer = layers.find(
+  }
+
+  initRoadsideObjectsLayer() {
+    if (!this.layers || !this.player) return;
+
+    const roadsideObjectsLayer = this.layers.find(
       (layer) => layer.layer.name === 'roadside_objects'
     )!;
-    const coinsLayer = layers.find((layer) => layer.layer.name === 'coins')!;
     this.physics.add.collider(this.player, roadsideObjectsLayer);
     roadsideObjectsLayer.setCollisionByExclusion([-1]);
+  }
 
+  initCoinsLayer() {
+    if (!this.layers || !this.player) return;
+
+    const coinsLayer = this.layers.find(
+      (layer) => layer.layer.name === 'coins'
+    )!;
     this.physics.add.collider(this.player, coinsLayer, (_, tile) => {
       tile.destroy();
       coinsLayer.removeTileAt(
@@ -86,6 +185,22 @@ export class Track extends Phaser.Scene {
       this.scoreText?.setText(`Score: ${this.score}`);
     });
     coinsLayer.setCollisionByExclusion([-1]);
+  }
+
+  preload() {
+    this.loadAssets();
+  }
+
+  create() {
+    this.createMap();
+    this.createPlayer();
+    this.addScoreText();
+    this.initCamera();
+    this.initWorldPhisics();
+    this.initRoadsideObjectsLayer();
+    this.initCoinsLayer();
+    this.buttonLeft = this.addButton('left');
+    this.buttonRight = this.addButton('right');
   }
 
   update(_: number, delta: number): void {
